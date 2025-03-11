e53b9f407e262dd93cb5f4e977c1ee9daf6c1f9c
anjfkrhgkwl <skskgus7516@gmail.com>
2025-03-11 23:06:14 +0900

    main

---
+++ js/index.js
@@ -0,0 +1,51 @@
+$(function(){
+    $('a').click(function(event){
+        event.preventDefault();
+    }) //클릭 이벤트 무시
+});
+
+$(function(){
+    let pickList = $('.pick-list' );
+    let pickListImage = $('.pick-list-img li')
+
+    pickListImage.not('.on').hide();
+
+    pickList.mouseenter(function(){
+        let index = $(this).index();
+
+        pickList.removeClass('click');
+        pickListImage.removeClass('on');
+
+        $(this).addClass('click');
+        pickListImage.eq(index).addClass('on');
+        pickListImage.eq(index).show();
+    })
+
+    /* pickList.mouseleave(function(){
+        pickList.removeClass('click');
+        pickListImage.removeClass('on');
+
+        pickList.eq(0).addClass('click');
+        pickListImage.eq(0).addClass('on');
+    }) */
+
+
+
+    let selectTheaterBtn = $('.todays-movie .select-theater-btn')
+    let theaterList = $('.todays-movie .select-theater')
+    let listColor = $('.todays-movie .select-theater li')
+
+    selectTheaterBtn.click(function(){
+        theaterList.slideToggle(500);
+        theaterList.toggleClass('on');
+    })
+
+    
+    listColor.mouseenter(function(){
+        listColor.removeClass('on')
+        $(this).addClass('on');
+    })
+    listColor.mouserleave(function(){
+        listColor.removeClass('on');
+    })
+});
